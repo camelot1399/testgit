0a4039d9becc984155359f2192c7fa8c80b89f47
alexei <alexei3017@mail.ry>
2022-03-01 12:14:46 +0300

    step 1.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="home">
-    step1
-  </div>
+    <div class="home">
+        step1
+    </div>
 </template>
 
 <script>
 
 export default {
-  name: 'Home',
-  components: {
-  }
+    name: 'Home',
+    components: {}
 }
 </script>
